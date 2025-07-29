bb0288b5f5d22b1453b13438dbc94d35b85f3ea0
Chaitanya-784 <chaitanyagholap784@gmail.com>
2025-07-29 16:28:09 +0530

    host changes

--- backend/chat.py
+++ backend/chat.py
@@ -219,4 +219,4 @@ def handle_message(data):
         return 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
